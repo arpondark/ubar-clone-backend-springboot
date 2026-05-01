f59d6d69fb0f5708c9de929bd97b8a41dc95fb2e
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-05-02 01:00:34 +0600

    fixed the version

--- maching-service/src/main/java/site/shazan/maching_service/client/LocationServiceCLient.java
+++ maching-service/src/main/java/site/shazan/maching_service/client/LocationServiceCLient.java
@@ -1,14 +1,14 @@
 package site.shazan.maching_service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import site.shazan.maching_service.dtos.NearByDriverResponse;
 
 import java.util.List;
 
 @FeignClient(name = "location-service", url = "${location.service.url}")
-@Service
 public interface LocationServiceCLient {
+    @GetMapping("/api/v1/locations/drivers/nearby")
     List<NearByDriverResponse> getNearByDrivers(@RequestParam double lattitude, @RequestParam double longitude, @RequestParam double radius);
 }
