497ca5c5f0752c162d80e27f35a9c57eaad21fe3
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-05-01 22:47:01 +0600

    dependency added

--- maching-service/src/main/java/site/shazan/maching_service/event/RideMachedEvent.java
+++ maching-service/src/main/java/site/shazan/maching_service/event/RideMachedEvent.java
@@ -1,4 +1,17 @@
 package site.shazan.maching_service.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RideMachedEvent {
+    private String riderId;
+    private String rideId;
+    private String driverId;
+    private double driverLatitude;
+    private double driverLongitude;
+    private double disthanseToPickupKm;
 }
