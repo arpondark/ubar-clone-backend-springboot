7307995b2920cfba95cdc98973ade577e14da18f
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-05-01 19:48:15 +0600

    kafka  config added

--- ride-service/src/main/java/site/shazan/ride_service/config/KafkaConfig.java
+++ ride-service/src/main/java/site/shazan/ride_service/config/KafkaConfig.java
@@ -1,7 +1,26 @@
 package site.shazan.ride_service.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
+    @Bean
+    public NewTopic rideResetTopic() {
+        return TopicBuilder.name("ride-requested")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic rideMachedTopic(){
+        return TopicBuilder.name("ride-matched")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
 }
