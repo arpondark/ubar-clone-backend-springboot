f8d6ffb866fef0b2c4d5795dcb64af7455ce920e
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-05-02 00:25:43 +0600

    configaration fixed

--- maching-service/src/main/java/site/shazan/maching_service/MachingServiceApplication.java
+++ maching-service/src/main/java/site/shazan/maching_service/MachingServiceApplication.java
@@ -2,8 +2,10 @@ package site.shazan.maching_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MachingServiceApplication {
 
 	public static void main(String[] args) {
